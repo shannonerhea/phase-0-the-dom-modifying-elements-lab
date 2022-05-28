6a3555e8e20559a5b5d8d74c6e0f40c277005723
shannone <shannone426@gmail.com>
2022-05-28 15:55:31 -0400

    dom passing 4

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Write your code here!
-const element = document.createElement("div");
-
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Jermey is the champion"
+document.body.append(newHeader);
