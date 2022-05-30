c1cc5033f6efab171f5cdabd55a95242bcb0d01c
shannone <shannone426@gmail.com>
2022-05-30 08:33:52 -0400

    update

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const main = document.getElementById("main");
 main.remove();
 const newHeader = document.createElement("h1");
-newHeader.id = "victory"
-newHeader.textContent = "Jermey is the champion"
+newHeader.id = "victory";
+newHeader.textContent = "Shannone is the champion";
 document.body.append(newHeader);
